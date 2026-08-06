4d06a9ab4b689ce27840f8398763c3004891043f
Faheem Falan <gmohammedfaheem@gmail.com>
2026-08-06 15:28:46 +0530

    sample test-2

---
+++ find_large_number.js
@@ -0,0 +1,11 @@
+let arr = [10, 25, 8, 99, 45];
+
+let largest = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) {
+        largest = arr[i];
+    }
+}
+
+console.log(largest);
