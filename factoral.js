0668e14cc90d34089bcbed3289000275cfa1c265
Faheem Falan <gmohammedfaheem@gmail.com>
2026-08-10 12:03:19 +0530

    Add initial QA code

--- factoral.js
+++ factoral.js
@@ -6,3 +6,12 @@ for (let i = 1; i <= num; i++) {
 }
 
 console.log(fact);
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " Even");
+    } else {
+        console.log(i + " Odd");
+    }
+}
+
